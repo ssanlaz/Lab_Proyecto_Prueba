d3e2c552bd9ca5af7dea66f9af609cba26f833ed
SANDRA <sandra.sanchez.laz@gmail.com>
2024-04-29 23:59:01 +0200

    Soy el user2 y he añadido funcionalidad a los metodos

--- src/ModeloDao/CalculadoraDaoImpl.java
+++ src/ModeloDao/CalculadoraDaoImpl.java
@@ -5,31 +5,31 @@ public class CalculadoraDaoImpl implements CalculadoraDao {
 	@Override
 	public int sumar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return num1+num2;
 	}
 
 	@Override
 	public int restar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return num1-num2;
 	}
 
 	@Override
 	public int multiplicar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return num1*num2;
 	}
 
 	@Override
 	public int dividir(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return num1/num2;
 	}
 
 	@Override
 	public int resto(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return num1%num2;
 	}
 
 }
